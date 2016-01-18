f50f5be881ad38381a70e5c90d9afd26aa456532
Alexander Maslennikov <almas.dev@gmail.com>
2016-01-18 15:28:49 +0300

    Initial Commit

---
+++ VIPER-Swift-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIPER-Swift-example
+//
+//  Created by Alexander Maslennikov on 18.01.16.
+//  Copyright © 2016 handh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
