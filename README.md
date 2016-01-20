22e89ece79df1c97dda393545aeaf077f35b9475
Alexander Maslennikov <almas.dev@gmail.com>
2016-01-20 13:49:14 +0300

    Add Typhoon

---
+++ README.md
@@ -0,0 +1,8 @@
+# Viper-Swift-example
+
+Viper example written in Swift
+
+## Requirements
+
+- iOS 8.0 + 
+- Swift 2.1
--- VIPER-Swift-example/ViewController.swift
+++ VIPER-Swift-example/ViewController.swift
@@ -1,7 +1,4 @@
 //
-//  ViewController.swift
-//  VIPER-Swift-example
-//
 //  Created by Alexander Maslennikov on 18.01.16.
 //  Copyright © 2016 handh. All rights reserved.
 //
@@ -12,14 +9,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        view.backgroundColor = UIColor.greenColor()
     }
-
 
 }
-
